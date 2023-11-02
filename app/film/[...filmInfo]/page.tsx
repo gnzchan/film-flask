42a4768b335db551be173904a8d6d516278e10a1
chan-gonzales <chrisgonzales.online@gmail.com>
2023-11-02 20:00:20 +0800

    Test metadata for filminfo

--- app/film/[...filmInfo]/page.tsx
+++ app/film/[...filmInfo]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata, ResolvingMetadata } from "next";
 import getFilmById from "@/actions/getFilmById";
 import FilmInfo from "@/components/ui/FilmInfo";
 import FilmTitleBar from "@/components/ui/FilmTitleBar";
@@ -8,6 +9,29 @@ interface FilmProps {
   params: {
     category: string;
     filmInfo: [FilmCategory, string];
+  };
+}
+
+export async function generateMetadata(
+  { params }: FilmProps,
+  parent?: ResolvingMetadata,
+): Promise<Metadata> {
+  const film = await getFilmById(params.filmInfo[0], params.filmInfo[1]);
+
+  return {
+    title: `${film.title ?? film.name} - Film Flask`,
+    openGraph: {
+      title: `${film.title ?? film.name} - Film Flask`,
+      description:
+        "${film.title ?? film.name}: Watch film, read overview, change status, and leave a review.",
+      url: `https://film-flask.vercel.app/film/${film.category}/${film.id}`,
+      siteName: "Film Flask",
+      images: [
+        {
+          url: `https://image.tmdb.org/t/p/w1280/${film.backdrop_path}`,
+        },
+      ],
+    },
   };
 }
 
