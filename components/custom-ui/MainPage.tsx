a406fb0ea77dfbacbcba3c85b1a2617a99b080d7
chan-gonzales <chrisgonzales.online@gmail.com>
2023-11-10 15:46:51 +0800

    Fix height of bg

--- components/custom-ui/MainPage.tsx
+++ components/custom-ui/MainPage.tsx
@@ -11,7 +11,7 @@ const MainPage: React.FC<MainPageProps> = ({ children }) => {
       <div className="z-[10] hidden h-full lg:fixed lg:inset-y-0 lg:flex lg:w-72 lg:flex-col">
         <Sidebar />
       </div>
-      <main className="bg-neutral-100 transition duration-500 ease-in-out dark:bg-zinc-900 lg:pl-72">
+      <main className="min-h-full bg-neutral-100 transition duration-500 ease-in-out dark:bg-zinc-900 lg:pl-72">
         <div className="h-full">{children}</div>
       </main>
     </div>
